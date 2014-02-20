37077f9c0baa4884d2b446d7983a58ed50f60e78
david <d.pereiro@udc.es>
2014-02-20 21:39:31 +0100

    Mejorada la interfaz por linea de comandos

--- build.sbt
+++ build.sbt
@@ -14,6 +14,7 @@ libraryDependencies ++= Seq(
   "com.typesafe.akka" %% "akka-testkit" % "2.2.3",
   "org.scalatest" %% "scalatest" % "2.0" % "test",
   "io.spray" % "spray-routing" % "1.2.0" % "test",
-  "io.spray" % "spray-client" % "1.2.0")
+  "io.spray" % "spray-client" % "1.2.0",
+  "org.rogach" %% "scallop" % "0.9.5")
 
 atmosSettings
--- src/main/scala/es/udc/prototype/Main.scala
+++ src/main/scala/es/udc/prototype/Main.scala
@@ -4,6 +4,8 @@ import akka.actor.{ActorRef, PoisonPill, Props, ActorSystem}
 import com.typesafe.config.ConfigFactory
 import akka.contrib.pattern.ClusterSingletonManager
 import es.udc.prototype.util.SingletonProxy
+import org.rogach.scallop.{LazyScallopConf, ScallopOption}
+import org.rogach.scallop.exceptions._
 
 /**
  * User: david
@@ -26,22 +28,38 @@ trait Startup {
     system.actorOf(Props(classOf[BaseCrawler], new Extractor {
       def extractLinks(response: Response): Seq[String] = Seq()
       def extractInformation(response: Response): Unit = Unit
-    }))
+    }), "baseCrawler")
   }
 
   def initDownloader(system : ActorSystem) : ActorRef = {
-    system.actorOf(Props[Downloader])
+    system.actorOf(Props[Downloader], "downloader")
   }
 
   def initManager(system : ActorSystem, master : ActorRef, downloader : ActorRef, crawler : ActorRef) : ActorRef = {
-    system.actorOf(Props(classOf[Manager], master, downloader, crawler))
+    system.actorOf(Props(classOf[Manager], master, downloader, crawler), "manager")
   }
 }
 
+
 object Main extends Startup {
   def main(args: Array[String]) : Unit = {
-    val portIndex = args.indexOf("-p")
-    if(portIndex >= 0) System.setProperty("akka.remote.netty.tcp.port", args(portIndex + 1))
+    object Conf extends LazyScallopConf(args.toList) {
+      val port : ScallopOption[Int] = opt[Int](name = "port",
+                                               descr = "Init port of the node",
+                                               required = true,
+                                               validate = p => p > 0 && p < 65535)
+    }
+
+    Conf.initialize {
+      case Help(_) =>
+        Conf.printHelp()
+        return
+      case e : ScallopException =>
+        Conf.printHelp()
+        return
+    }
+
+    System.setProperty("akka.remote.netty.tcp.port", Conf.port().toString)
 
     val system = ActorSystem("ClusterSystem", ConfigFactory.load("application.conf"))
     val master = initMaster(system)
