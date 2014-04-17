48597a40429e1fb720632949bd46a757515c5e20
david <d.pereiro@udc.es>
2014-04-17 20:17:20 +0200

    Fix #23 Reintentar códigos de error HTTP.

--- src/main/scala/es/udc/prototype/pipeline/RetryHttpError.scala
+++ src/main/scala/es/udc/prototype/pipeline/RetryHttpError.scala
@@ -1,5 +1,41 @@
 package es.udc.prototype.pipeline
 
-class RetryHttpError {
+import es.udc.prototype.{Response, Request}
+import com.typesafe.config.Config
+import scala.collection.mutable.{Map => MMap}
+import spray.http.StatusCode
+import scala.collection.JavaConversions._
+import akka.actor.ActorLogging
 
+class RetryHttpError(config: Config) extends Stage with ActorLogging {
+  val requests = MMap[String, (Request, Int)]()
+
+  val toFilter: Set[StatusCode] = config.getIntList("prototype.retry-http-error.errors").toSet[Integer]
+    .map(i => StatusCode.int2StatusCode(i.intValue))
+  val maxRetries: Int = config.getInt("prototype.retry-http-error.max-retries")
+
+  override def active: Receive = {
+    case request: Request =>
+      requests.put(request.task.id, (request, 0))
+      right ! request
+    case response: Response =>
+      if (requests.contains(response.task.id)) {
+        val (prevRequest, retries) = requests(response.task.id)
+        if (toFilter.contains(response.status)) {
+          if (retries < maxRetries) {
+            log.info(s"Retrying request ${response.task.id} with code ${response.status}")
+            right ! prevRequest
+          } else {
+            log.warning(s"Max requests reached for response ${response.task.id}")
+          }
+        } else {
+          // Response code correct, pass to the next stage and clear from Map
+          requests.remove(response.task.id)
+          left ! response
+        }
+      } else {
+        // Unknown response, drop it
+        log.warning(s"Dropping an unknown response with id ${response.task.id}")
+      }
+  }
 }
--- src/test/scala/es/udc/prototype/pipeline/RetryHttpErrorTest.scala
+++ src/test/scala/es/udc/prototype/pipeline/RetryHttpErrorTest.scala
@@ -1,5 +1,68 @@
 package es.udc.prototype.pipeline
 
-class RetryHttpErrorTest {
+import akka.testkit.{TestProbe, ImplicitSender, TestKit}
+import akka.actor.{Props, ActorSystem}
+import com.typesafe.config.ConfigFactory
+import org.scalatest.{BeforeAndAfterAll, Matchers, WordSpecLike}
+import scala.collection.JavaConversions._
+import es.udc.prototype.{Task, Request, Response}
+import spray.http.{StatusCode, Uri}
+
+class RetryHttpErrorTest extends TestKit(ActorSystem("test-system", ConfigFactory.load("application.test.conf")))
+with ImplicitSender
+with WordSpecLike
+with Matchers
+with BeforeAndAfterAll {
+
+  override def afterAll() {
+    system.shutdown()
+  }
+
+  val config = ConfigFactory.load("application.test.conf")
+  val errors = config.getIntList("prototype.retry-http-error.errors").toList
+  val allowed = Seq(500, 403)
+
+  def initRetry() = {
+    val retry = system.actorOf(Props(classOf[RetryHttpError], config))
+    val listener = TestProbe()
+    val left = TestProbe()
+    val right = TestProbe()
+    listener.send(retry, new LeftRight(left.ref, right.ref))
+    listener.expectMsg(Initialized)
+    (retry, left, right)
+  }
+
+  "A RetryHttpErrorStage" should {
+    "retry the configured http errors, sending again the Request" in {
+      val (retry, left, right) = initRetry()
+      val task = new Task("id", Uri.Empty, 0)
+      errors.foreach {
+        e =>
+          left.send(retry, new Request(task, Map()))
+          right.expectMsg(new Request(task, Map()))
+          right.send(retry, new Response(task, StatusCode.int2StatusCode(e), Map(), ""))
+          right.expectMsg(new Request(task, Map()))
+      }
+    }
+
+    "allow the rest of the error codes" in {
+      val (retry, left, right) = initRetry()
+      val task = new Task("id", Uri.Empty, 0)
+      allowed.foreach {
+        e =>
+          left.send(retry, new Request(task, Map()))
+          right.expectMsg(new Request(task, Map()))
+          right.send(retry, new Response(task, StatusCode.int2StatusCode(e), Map(), ""))
+          left.expectMsg(new Response(task, StatusCode.int2StatusCode(e), Map(), ""))
+      }
+    }
+
+    "drop unknown Responses, it should be restarted later" in {
+      val (retry, left, right) = initRetry()
+      val task = new Task("id", Uri.Empty, 0)
+      right.send(retry, new Response(task, 200, Map(), ""))
+      left.expectNoMsg()
+    }
+  }
 
 }
