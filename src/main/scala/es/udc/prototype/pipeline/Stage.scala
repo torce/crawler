1c25a6f060c0e37ac5a843f27ecbddc3a0965a28
david <d.pereiro@udc.es>
2014-05-04 18:34:11 +0200

    Fix #30 Las etapas del pipeline pueden modificar su estructura en tiempo de ejecución

--- src/main/scala/es/udc/prototype/pipeline/Stage.scala
+++ src/main/scala/es/udc/prototype/pipeline/Stage.scala
@@ -13,15 +13,19 @@ case class LeftRight(left: ActorRef, right: ActorRef)
 case object Initialized
 
 trait Stage extends Actor {
-  var left: ActorRef = _
-  var right: ActorRef = _
+  private var _left: ActorRef = _
+  private var _right: ActorRef = _
+
+  def left = _left
+
+  def right = _right
 
   def active: Actor.Receive
 
   final def receive = {
     case LeftRight(l, r) =>
-      left = l
-      right = r
+      _left = l
+      _right = r
       sender ! Initialized
       context.become(active)
   }
